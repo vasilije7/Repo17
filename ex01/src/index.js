639acb2bdd83d9c1e5df8ef50717c77faad2d1bf
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-13 15:07:26 +0200

    Vjezba 1

---
+++ ex01/src/index.js
@@ -0,0 +1,10 @@
+const array1 = ["MON", "TUE", "WED", "THU", "FRI"];
+
+function main(arr) {
+  // Only change code below this line
+  let array2 = [...array1]; // Change this line
+  // Only change code above this line
+  return array2;
+}
+console.log(main(array1));
+module.exports = main;
