5cfcf51e4e44d1285f4ab961863b855bc9270868
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-13 21:11:30 +0200

    Vjezba 6 odradjena i dodata ex07

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -2,7 +2,7 @@ const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function removeFirstTwo(list) {
   "use strict";
   // Only change code below this line
-  const [] = []; //Change this line
+  const [a, b, ...array2] = list; //Change this line
   // Only change code above this line
   return array2;
 }
---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+const range = {
+  max: 100.0,
+  min: -25.0,
+  num1: 17.5,
+  num2: 88.8,
+  average: 45.325,
+};
+// Only change code below this line
+const middle = (range) => {};
+// Only change code below this line
+console.log(middle(range));
+module.exports = middle;
